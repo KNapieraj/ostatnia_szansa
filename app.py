566d1b4f22fa4b997174d18c051f6fe4c13e6376
Kamil Napieraj <kamil@email.com>
2022-04-24 10:36:41 +0200

    Dodanie pliku app

---
+++ app.py
@@ -0,0 +1,14 @@
+"""Witam w pliku app"""
+
+from flask import Flask
+
+APP = Flask(__name__)
+
+@APP.route('/')
+
+def index():
+    """Funkcja"""
+    return '<h1>Hello WSB! Greetings from Flask & Docker!</h1>'
+
+if __name__ == "__main__":
+    APP.run(debug=True)
