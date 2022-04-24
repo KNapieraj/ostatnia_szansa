cc583d93371fca26bea98cedc132c4865c54c087
Kamil Napieraj <kamil@email.com>
2022-04-24 11:22:32 +0200

    Dodanie pytestow w app.py

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
 """Witam w pliku app"""
 
-from flask import Flask
+# from flask import Flask
 
-APP = Flask(__name__)
+# APP = Flask(__name__)
 
-@APP.route('/')
+# @APP.route('/')
 
-def index():
-    """Funkcja"""
-    return '''<h1><center>Witam na mojej stronie</center></h1>\n
-            <h2><center>Kamil Napieraj<center></h2>'''
+# def index():
+#     """Funkcja"""
+#     return '''<h1><center>Witam na mojej stronie</center></h1>\n
+#             <h2><center>Kamil Napieraj<center></h2>'''
 
-if __name__ == "__main__":
-    APP.run(debug=True)
+def dodawanie(a):
+    """Proste dodawanie"""
+    return a + 1
+
+def test():
+    """Prosty test"""
+    assert dodawanie(4) == 5
+    assert dodawanie(5) == 6
+
+# if __name__ == "__main__":
+    # APP.run(debug=True)
